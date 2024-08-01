7988fdfa22839fe9099f2c9c58d1908229873982
amitcjc <amitcjcpune@gmail.com>
2024-08-01 10:08:43 +0530

    by xyz

--- GitDemoB178/src/com/cjc/Demo.java
+++ GitDemoB178/src/com/cjc/Demo.java
@@ -2,7 +2,15 @@ package com.cjc;
 
 public class Demo
 {
-public static void main(String[] args) {
+	
+	public void m1()
+	{
+		System.out.println("This is m1");
+	}
+	
+	
+public static void main(String[] args)
+{
 	
 	System.out.println("This is Main Method");
 }
